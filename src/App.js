0f09fecf88d192db70556795f3ff5cbec8aecb48
minju0423 <lmj042398@gmail.com>
2024-09-09 16:14:51 +0900

    🚧 Add : App.js - Routes 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import ComplimentList from './Pages/ComplimentList'
+import UserList from './Pages/UserList'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/compliments' element={<ComplimentList/>}></Route>
+        <Route path='/users' element={<UserList/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
