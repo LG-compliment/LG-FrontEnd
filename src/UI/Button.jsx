f2065ec97ef54c69ba284b1c571c62e886e6813f
minju0423 <lmj042398@gmail.com>
2024-09-11 09:41:08 +0900

    💄 Add : Button, Input Component

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 
-function Button() {
+const Btn = styled.button`
+  width: 80px;
+  height: 40px;
+  background-color: #EB008B;
+  color: white;
+  border: #EB008B;
+  border-radius: 5px;
+  font-size: 15px;
+  font-weight: bold;
+  
+`
+
+function Button(props) {
   return (
-    <div>Button</div>
+    <Btn>{props.title}</Btn>
   )
 }
 
