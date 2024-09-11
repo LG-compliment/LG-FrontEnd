221b80963b013e2d74d8c836a278a100a1b65cef
minju0423 <lmj042398@gmail.com>
2024-09-11 11:24:03 +0900

    💄 Update : Input width 변경

--- src/UI/Input.jsx
+++ src/UI/Input.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import styled from 'styled-components';
 
 const InputText = styled.input`
-  width: 250px;
-  height: 34px; //default : border 2 padding 1 
+  width: ${(props) => props.width || '250px'}; //props로 길이를 받지 않으면 default(=250px) 
+  height: 36px; //버튼이랑 height 통일
   margin-right: 5px;
+  border:1px solid #D9D9D9;
+  border-radius: 5px;
+  padding-left: 10px;
 
 `
 
 function Input(props) {
   return (
-    <InputText placeholder={props.placeholder}/>
+    <InputText placeholder={props.placeholder} width={props.width}/>
   )
 }
 
