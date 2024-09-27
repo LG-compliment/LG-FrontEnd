3ffb65487ad87c8626f1edda138d4a83b4d1abad
minju0423 <lmj042398@gmail.com>
2024-09-27 11:37:03 +0900

    🚧 Fix : 아이디, 이름 유효성검사

--- src/Pages/Login/SignUp.jsx
+++ src/Pages/Login/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styled, { createGlobalStyle, css, keyframes } from 'styled-components';
 import Input from '../../ui/Input';
 import Button from '../../ui/Button';
-import { CircleArrowLeft, ShieldCheck } from 'lucide-react';
+import { CircleArrowLeft, ShieldCheck, Info } from 'lucide-react';
 import { checkId, signUp } from '../../api/api';
 
 
@@ -53,6 +53,9 @@ const SignUpForm = styled.div`
 const ItemTitle = styled.div`
     font-size: 12px;
     align-self: flex-start;
+    display: flex;
+    align-items: center;
+    position: relative; /* 추가 */
 `
 
 const SignUpButton = styled(Button)`
@@ -76,11 +79,11 @@ const BackIcon = styled(CircleArrowLeft)`
 `
 
 const CheckBtn = styled(({ isavailable, isChecked, ...rest }) => <Button {...rest} />)`
-  background-color: ${props => props.isavailable ? '#4CAF50' : props.isChecked ? '#FF4136' : '#EB008B'};
+  background-color: ${props => props.isChecked ? '#4CAF50' : props.isavailable ? '#FF4136' : '#EB008B'};
   width: 40px;
   height: 40px;
   &:hover {
-    background-color: ${props => props.isavailable ? '#4CAF50' : props.isChecked ? '#FF4136' : '#d10079'};
+    background-color: ${props => props.isChecked ? '#4CAF50' : props.isavailable ? '#FF4136' : '#d10079'};
   }
 
   &:active {
@@ -89,7 +92,7 @@ const CheckBtn = styled(({ isavailable, isChecked, ...rest }) => <Button {...res
 `;
 
 
-const ItemWrapper = styled.div`
+const ItemContainer = styled.div`
     display: flex;
     align-items: center;
 `
@@ -141,9 +144,7 @@ const CheckText = styled.span`
   font-size: 12px;
   cursor: pointer;
   margin-top: 5px;  // 버튼과 텍스트 사이의 간격
-  &:hover {
-    text-decoration: underline;
-  }
+  
 `;
 
 const CheckWrapper = styled.div`
@@ -153,6 +154,42 @@ const CheckWrapper = styled.div`
   margin-left: 10px;  // 아이디 입력 필드와 버튼 사이의 간격
 `;
 
+const InfoIcon = styled(Info)`
+    color: #EB008B;
+    margin-inline-start: 3px;
+    cursor: pointer;
+`;
+
+const InfoTooltip = styled.div`
+  position: absolute;
+  top: 50%; /* 아이콘의 수직 중앙에 맞추기 */
+  left: 100%; /* 아이콘의 오른쪽에 위치하도록 설정 */
+  transform: translateY(-50%); /* 수직 중앙 정렬 */
+  background-color: #EB008B; /* Compliment Hub 색상 */
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 12px;
+  white-space: nowrap;
+  z-index: 10;
+  opacity: ${props => (props.$isVisible ? 1 : 0)};
+  transition: opacity 0.3s ease;
+  pointer-events: none;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 50%; /* 삼각형의 수직 중앙에 맞추기 */
+    left: -5px; /* 삼각형을 툴팁 왼쪽에 위치 */
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent #EB008B transparent transparent; /* 툴팁과 같은 색상으로 삼각형 설정 */
+  }
+`;
+
+
+
 function SignUp() {
     const [id, setId] = useState('');
     const [name, setName] = useState('');
@@ -161,6 +198,9 @@ function SignUp() {
     const [isIdAvailable, setIsIdAvailable] = useState(false);
     const [isIdChecked, setIsIdChecked] = useState(false);
     const [showTooltip, setShowTooltip] = useState(false);
+    const [isIdTooltipVisible, setIsIdTooltipVisible] = useState(false);
+    const [isNameTooltipVisible, setIsNameTooltipVisible] = useState(false);
+
 
     useEffect(() => {
         let timer;
@@ -176,17 +216,37 @@ function SignUp() {
         window.location.href = '/login';
     }
 
+    const validateId = (id) => { 
+        const idPattern = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{4,12}$/; // 영문 대소문자와 숫자를 모두 포함해야 함, 4~12자
+        return idPattern.test(id);
+    };
+    const validateName = (name) => {
+        const namePattern = /^[가-힣]+$/; // 한글만
+        return namePattern.test(name);
+    };
+
     const handleCheckId = async (e) => {
         e.preventDefault();
         if (!id) {
             alert('아이디를 입력해주세요.');
             return;
         }
+
+        if (!validateId(id)) { //유효성검사가 맞지 않을 경우 툴팁 보여줌
+            setIsIdTooltipVisible(true)
+            setTimeout(()=>{
+                setIsIdTooltipVisible(false)
+            },1000)
+            return;
+        }
+
         try {
-            const response = await checkId(id);
-            setIsIdAvailable(response.data.available);
-            setIsIdChecked(true);
-            setShowTooltip(true);
+            if(!isIdChecked){ //중복체크가 false일때만 실행
+                const response = await checkId(id);
+                setIsIdAvailable(response.data.available);
+                setIsIdChecked(true);
+                setShowTooltip(true);
+            }
         } catch (error) {
             console.error("Error checking ID:", error);
             setIsIdAvailable(false);
@@ -209,6 +269,14 @@ function SignUp() {
             alert('모든 필드를 입력해주세요.');
             return;
         }
+        if(!validateName(name)){
+            
+            setIsNameTooltipVisible(true)
+            setTimeout(()=>{
+                setIsNameTooltipVisible(false)
+            },1000)
+            return;
+        }
         try {
             const response = await signUp(id, password, name);
             alert('회원가입이 완료되었습니다!');
@@ -227,8 +295,18 @@ function SignUp() {
                     <BackIcon onClick={clickBack}></BackIcon>
                     <SignUpTitle>Sign Up</SignUpTitle>
                     <SignUpForm>
-                        <ItemTitle>아이디</ItemTitle>
-                        <ItemWrapper>
+                        <ItemTitle>아이디 
+                            <InfoIcon
+                                size={15}
+                                onMouseEnter={() => setIsIdTooltipVisible(true)}
+                                onMouseLeave={() => setIsIdTooltipVisible(false)}
+                            />
+                            {/* 툴팁 */}
+                            <InfoTooltip $isVisible={isIdTooltipVisible}>
+                                아이디는 영문 대소문자와 숫자를 모두 포함해야 하며, 길이는 4~12자여야 합니다.
+                            </InfoTooltip>
+                        </ItemTitle>
+                        <ItemContainer>
                             <InputCustom 
                                 placeholder="아이디를 입력해주세요"
                                 onChange={(event) => {
@@ -236,7 +314,7 @@ function SignUp() {
                                     setIsIdChecked(false);
                                 }}
                                 value={id}
-                            />
+                            />     
                             <CheckWrapper>
                                 <CheckBtn 
                                     onClick={handleCheckId}
@@ -250,8 +328,19 @@ function SignUp() {
                                 </CheckBtn>
                                 <CheckText onClick={handleCheckId}>중복 확인</CheckText>
                             </CheckWrapper>
-                        </ItemWrapper>
-                        <ItemTitle>이름</ItemTitle>
+                           
+                        </ItemContainer>
+                        <ItemTitle>이름
+                        <   InfoIcon
+                                size={15}
+                                onMouseEnter={() => setIsNameTooltipVisible(true)}
+                                onMouseLeave={() => setIsNameTooltipVisible(false)}
+                            />
+                            {/* 툴팁 */}
+                            <InfoTooltip $isVisible={isNameTooltipVisible}>
+                                이름은 한글만 입력할 수 있습니다.
+                            </InfoTooltip>
+                        </ItemTitle>
                         <InputCustom 
                             placeholder="이름을 입력해주세요" 
                             onChange={(event) => setName(event.target.value)}
